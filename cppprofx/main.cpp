40ccfc71566a8d8411996e122d9074ca823ca42b
Dmytriy Aviedikov <avedikovdima@mail.ru>
2017-05-03 19:44:24 +0300

    fourth commit

--- cppprofx/main.cpp
+++ cppprofx/main.cpp
@@ -8,7 +8,6 @@ void main()
 	LONG_INT li1("1232432435598983843587485"), li2("395885473865763764576374");
 	LONG_INT li3;
 	li3 = li1 * li2;
-	cout << li1 << endl;
 	cout << li2 << endl;
 	cout << li3 << endl;
 	cout << "secondBranch" << endl;
